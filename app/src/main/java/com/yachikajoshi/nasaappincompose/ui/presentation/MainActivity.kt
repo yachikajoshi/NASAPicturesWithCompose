bda06c150604dff43f3586909bbc814d79072a51
Yachika Joshi <yachika@obvious.in>
2023-04-20 16:47:38 +0530

    Add transition animation

--- app/src/main/java/com/yachikajoshi/nasaappincompose/ui/presentation/MainActivity.kt
+++ app/src/main/java/com/yachikajoshi/nasaappincompose/ui/presentation/MainActivity.kt
@@ -3,23 +3,26 @@ package com.yachikajoshi.nasaappincompose.ui.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.navigation
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.yachikajoshi.nasaappincompose.ui.theme.NasaAppInComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NasaAppInComposeTheme {
                 val viewModel = hiltViewModel<MainViewModel>()
 
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "pictureList") {
+                val navController = rememberAnimatedNavController()
+                AnimatedNavHost(navController = navController, startDestination = "pictureList") {
                     composable("pictureList") {
                         PictureList(
                             mainUiState = viewModel.uiState.value,
